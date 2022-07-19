e972842bc6b1fbde3816a57a7b53081d285eb20d
naderright <nadercs2018@gmail.com>
2022-07-19 17:11:20 +0200

    add slider

--- src/componnent/HomeScreen/HomeScreen.jsx
+++ src/componnent/HomeScreen/HomeScreen.jsx
@@ -10,14 +10,14 @@ function HomeScreen() {
     <div className='HomeScreen'>
       <Navbar/>
       <Banner/>
-      <Row title='NETFLIX ORIGINALS' fetchUrl={Requests.fetchNetflixOriginals} largRow/>
-      <Row title='Trending Movie' fetchUrl={Requests.fetchTrending}/>
-      <Row title='Top Rated' fetchUrl={Requests.fetchTopRated}/>
-      <Row title='Action movies' fetchUrl={Requests.fetchActionMovies}/>
-      <Row title='Comedy movies' fetchUrl={Requests.fetchComedyMovies}/>
-      <Row title='Horror Movies' fetchUrl={Requests.fetchHorrorMovies}/>
-      <Row title='Romance Movies' fetchUrl={Requests.fetchRomanceMovies}/>
-      <Row title='Documentaries' fetchUrl={Requests.fetchDocumentaries }/>
+      <Row sd='1' title='NETFLIX ORIGINALS' fetchUrl={Requests.fetchNetflixOriginals} largRow/>
+      <Row sd='2' title='Trending Movie' fetchUrl={Requests.fetchTrending}/>
+      <Row sd='3' title='Top Rated' fetchUrl={Requests.fetchTopRated}/>
+      <Row sd='4' title='Action movies' fetchUrl={Requests.fetchActionMovies}/>
+      <Row sd='5' title='Comedy movies' fetchUrl={Requests.fetchComedyMovies}/>
+      <Row sd='6' title='Horror Movies' fetchUrl={Requests.fetchHorrorMovies}/>
+      <Row sd='7' title='Romance Movies' fetchUrl={Requests.fetchRomanceMovies}/>
+      <Row sd='8' title='Documentaries' fetchUrl={Requests.fetchDocumentaries }/>
     </div>
   )
 }
--- src/componnent/Rows/Row.jsx
+++ src/componnent/Rows/Row.jsx
@@ -2,7 +2,9 @@ import axios from '../axios/axios';
 import React, { useEffect, useState } from 'react'
 import './Rowi.css'
 import Requests from '../axios/Requests';
-function Row({ title, fetchUrl, largRow = false }) {
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+
+function Row({ title, fetchUrl, largRow = false ,sd}) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
@@ -14,19 +16,43 @@ function Row({ title, fetchUrl, largRow = false }) {
 
   }, [fetchUrl]);
   // console.log(movies);
+  const slidRight =()=>{
+    let slider = document.getElementById(`slid ${sd}`);
+    let icon = document.getElementById(`scroll ${sd}`);
+    // console.log(slider.scrollLeft);
+    slider.scrollLeft = slider.scrollLeft + 500 ;
+    icon.style.display= 'block'
+
+  }
+
+  const slidLeft =()=>{
+    let slider = document.getElementById(`slid ${sd}`);
+    let icon = document.getElementById(`scroll ${sd}`);
+    if (slider.scrollLeft === 500 ) {
+      slider.scrollLeft = slider.scrollLeft - 500 ;
+      icon.style.display= 'none'
+    }else{
+      slider.scrollLeft = slider.scrollLeft - 500 ;
+    }
+      
+  };
+
+
   return (
 
     <div className='row'>
       <h2>{title}</h2>
-      <div className="row_posters">
+      <FaAngleLeft className='scrollRight' id={`scroll ${sd}`} onClick={slidLeft} />
+      <div className="row_posters" id={`slid ${sd}`}>
+
         {movies.map((movie) =>
           // console.log(`${Requests.img}${movie.poster_path}`);
 
-          <img className={ `row_poster `} key={movie.id} alt={movie.name} src={`${Requests.img}${largRow ? movie.poster_path : movie.backdrop_path}`} />
+          <img className={`row_poster `} id={title === 'NETFLIX ORIGINALS'?'original':''} key={movie.id} alt={movie.name} src={`${Requests.img}${largRow ? movie.poster_path : movie.backdrop_path}`} />
 
         )}
       </div>
-
+      <FaAngleRight className='scrollLeft' onClick={slidRight}/>
     </div>
 
   )
